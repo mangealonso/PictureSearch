bf7ecf051c4f4e98e896a34306c62f2ea5da95a6
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-02-09 19:57:25 +0100

    Added files

---
+++ main.js
@@ -0,0 +1,23 @@
+let form =document.querySelector('form');
+
+form.onsubmit = async event => {
+    event.preventDefault();
+
+    let searchWord = form.word.value;
+
+
+
+    /* let url =
+    let response = await fetch(url);
+    let json = await response.json(); */
+};
+
+
+
+function previousPage(){
+
+}
+
+function nextPage(){
+
+}
