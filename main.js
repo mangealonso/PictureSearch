1bb59e1797877ce2f2bd3a53fdc72017163903c7
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-02-10 16:17:19 +0100

    Updated API code

--- main.js
+++ main.js
@@ -10,13 +10,15 @@ form.onsubmit = async event => {
     let searchWord = form.word.value;
     let chosenColor = form.color.value;
 
-    let url = 'https://pixabay.com/api/?key=33514834-460ad1b7211981a50c737ee93&q=black+cat&image_type=photo';
+    let url = 'https://pixabay.com/api/?key=33514834-460ad1b7211981a50c737ee93&q='+ chosenColor +'+'+ searchWord +' &image_type=photo';
     let response = await fetch(url);
     let json = await response.json();
+
+    console.log(json);
     
     // Clear the inputs
-    form.word.value = '';
-    form.color.value = '';
+    /* form.word.value = '';
+    form.color.value = ''; */
 };
 
 
