cf92d024fdb73aa747457ee804b8141652876fb7
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-02-10 15:11:38 +0100

    Added some API code

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-let form =document.querySelector('form');
+// Find the element
+let form = document.querySelector('form');
+
 
 form.onsubmit = async event => {
+    // Prevent the default "reload page" behavior
     event.preventDefault();
 
+    // Get the values entered by the user
     let searchWord = form.word.value;
+    let chosenColor = form.color.value;
 
-
-
-    /* let url =
+    let url = 'https://pixabay.com/api/';
     let response = await fetch(url);
-    let json = await response.json(); */
+    let json = await response.json();
+    
+    // Clear the inputs
+    form.word.value = '';
+    form.color.value = '';
 };
 
 
