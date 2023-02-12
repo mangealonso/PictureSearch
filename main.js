b8ff13f4a12b58678a057b159893aebc2c52ef22
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-02-12 22:10:34 +0100

    Rearranged thoughts and things still having to be fixed

--- main.js
+++ main.js
@@ -14,14 +14,24 @@ form.onsubmit = async event => {
     let response = await fetch(url);
     let json = await response.json();
 
+    // Har nedanstående kod bara för att se vad som händer efter att fråga skickats via API
     console.log(json);
+
+    // Loop with picture, user and tags for 10 objects to follow beneath this line:
     
     // Clear the inputs
     /* form.word.value = '';
     form.color.value = ''; */
 };
 
-
+//Things to be fixed and random thoughts:
+//read totalHits and store in a variable
+//decide how many objects that should be read from Pixabay(max 200) and stored. 
+//also decided whether the 10 pictures to be displayed should be read from Pixabay 
+//or from the amount stored
+//read url, user and tags and store as objects in a list?!
+//when nextPage-button has been clicked, enable previousPage-button and withdraw 10 from totalHits
+//if totalhits < 10, disable nextPage-button
 
 function previousPage(){
 
@@ -30,11 +40,3 @@ function previousPage(){
 function nextPage(){
 
 }
-
-//read totalHits and store in a variable
-//decide how many objects that should be read from Pixabay(max 200) and whether the 10 pictures 
-//to be displayed should be read from Pixabay or from the amount stored
-//read url, user and tags and store as objects in a list?!
-//show 10 pictures
-//when nextPage-button has been clicked, enable previousPage-button and withdraw 10 from totalHits
-//if totalhits < 10, disable nextPage-button
