2ce0e4e69f18e1abedd278a89b7ab742ed8c1756
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-02-15 09:06:20 +0100

    Formatted document

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let apiKey = '33514834-460ad1b7211981a50c737ee93';
 let totalHits = 0;
 let searchWord;
 let chosenColor;
-let counter = 0; 
+let counter = 0;
 
 /* function disableButton(button){
     document.querySelector(button).disabled = true;
@@ -23,13 +23,13 @@ form.onsubmit = async event => {
     chosenColor = form.color.value;
 
     //let url = 'https://pixabay.com/api/?key=33514834-460ad1b7211981a50c737ee93&q='+ chosenColor +'+'+ searchWord +' &image_type=photo&per_page=200';
-    
-    let url = 'https://pixabay.com/api/?key='+ apiKey + '&q='+ searchWord +'&colors=' + chosenColor + '&image_type=photo&per_page=10&page'
+
+    let url = 'https://pixabay.com/api/?key=' + apiKey + '&q=' + searchWord + '&colors=' + chosenColor + '&image_type=photo&per_page=10&page'
 
     if (chosenColor === "any color") {
-        url = 'https://pixabay.com/api/?key=' + apiKey +'&q='+ searchWord +'&image_type=photo&per_page=10&page';
+        url = 'https://pixabay.com/api/?key=' + apiKey + '&q=' + searchWord + '&image_type=photo&per_page=10&page';
     }
-   
+
     let response = await fetch(url);
     let json = await response.json();
     totalHits = json.totalHits;
@@ -42,7 +42,7 @@ form.onsubmit = async event => {
 
     // Har nedanstående kod bara för att se vad som händer efter att fråga skickats via API
     console.log(json.hits[0].tags);
-    
+
     // Clear the inputs
     /* form.word.value = '';
     form.color.value = ''; */
@@ -51,68 +51,64 @@ form.onsubmit = async event => {
     //showUser(json.hits);
     //showTags(json.hits);
 
-     counter++;
-    }
+    counter++;
+}
 
-    function showImages(jsonAnswer){
-        
-     // https://dev.to/satvik/how-to-fetch-images-from-and-api-5h8h - lite källa, som jag ändå fick ändra om allt efteråt.
-        const pics = document.querySelectorAll(".image");     
+function showImages(jsonAnswer) {
 
-        for (i = 0; i < 10; i++) {
-         
-         let imageFromPUrl = jsonAnswer[i].webformatURL;
+    // https://dev.to/satvik/how-to-fetch-images-from-and-api-5h8h - lite källa, som jag ändå fick ändra om allt efteråt.
+    const pics = document.querySelectorAll(".image");
+
+    for (i = 0; i < 10; i++) {
+
+        let imageFromPUrl = jsonAnswer[i].webformatURL;
         //  let imageEl = document.createElement("img"); //ev ta bort
         //  imageEl.setAttribute("src", imageFromPUrl);  //ev tabort
-         let currentImg = "#image" + i;
-         let pic = document.querySelector(currentImg);
-         
-         pics[i].setAttribute("src", imageFromPUrl); //tror att det är den här som gjorde att det till slut funkade.
-     };
+        let currentImg = "#image" + i;
+        let pic = document.querySelector(currentImg);
 
-     function showUser(jsonAnswer){
-        const users = document.querySelectorAll(".user");
+        pics[i].setAttribute("src", imageFromPUrl); //tror att det är den här som gjorde att det till slut funkade.
+    }
+};
 
-        for(i = 0; i < 10; i++){
+function showUser(jsonAnswer) {
+    const users = document.querySelectorAll(".user");
+
+    for (i = 0; i < 10; i++) {
         let userFromP = json.hits[i].user;
-         let currentUser = "#user" + i;
-         let user = document.querySelector(currentUser);
-         users[i].setAttribute("src", userFromP);
-        }
-     };
+        let currentUser = "#user" + i;
+        let user = document.querySelector(currentUser);
+        users[i].setAttribute("src", userFromP);
+    }
+};
 
-     function showTags(jsonAnswer){
-        const tags = document.querySelectorAll(".tag");
+function showTags(jsonAnswer) {
+    const tags = document.querySelectorAll(".tag");
 
-        for(i = 0; i < 10; i++){
+    for (i = 0; i < 10; i++) {
         let tagFromP = json.hits[i].tags;
-         let currentTag = "#tag" + i;
-         let t = document.querySelector(currentTag);
-         tags[i].setAttribute("src".tagFromP);
-        }
-     };
+        let currentTag = "#tag" + i;
+        let t = document.querySelector(currentTag);
+        tags[i].setAttribute("src".tagFromP);
+    }
+};
 
-    };
+let nextBtn = document.querySelector("#nextPage");
+let previousBtn = document.querySelector("#previousPage");
 
-    
+nextBtn.onclick = event => {
+    counter++
+}
 
-     let nextBtn = document.querySelector("#nextPage");
-     let previousBtn = document.querySelector("#previousPage");
-     
+previousBtn.onclick = event => {
+    counter--;
+}
 
-     nextBtn.onclick = event => {
-        counter++
-     }
 
-     previousBtn.onclick = event => {
-        counter--; 
-     }
-     
-
-function previousPage(){
+function previousPage() {
 
 }
 
-function nextPage(){
+function nextPage() {
 
 }
